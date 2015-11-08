b3102483eb67ee3f70ca8dadc538473c8f02c875
Siong <siong1987@gmail.com>
2015-11-08 13:06:52 -0800

    Revert "no encoding needed for video format."
    This reverts commit 5ef5a8cadcc403a37ec4ecb884909b345c8ad5be.

--- helpers/ffmpeg.js
+++ helpers/ffmpeg.js
@@ -8,8 +8,25 @@ module.exports = function (req, res, file) {
   function remux() {
     res.type('application/x-mpegURL');
     var command = ffmpeg(file.createReadStream())
-      .videoCodec('copy').audioCodec('aac').format('mp4')
-      .outputOptions('-movflags frag_keyframe+empty_moov')
+      .outputOptions([
+        '-vcodec libx264',
+        '-pix_fmt yuv420p',
+        '-segment_list_type m3u8',
+        '-map 0:v',
+        '-map 0:a:0',
+        '-c:a mp3',
+        '-ar 44100',
+        '-f hls',
+        '-hls_time 10',
+        '-hls_list_size 6',
+        '-hls_wrap 18',
+        '-start_number 1',
+        '-deinterlace',
+        '-threads 0',
+        '-loglevel quiet',
+        '-ac 2',
+        '-b:a 160000'
+      ])
       .on('start', function (cmd) {
         console.log(cmd);
       })
